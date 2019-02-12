37e6aeb8db7accb709664d0b70f23df9c2cde997
minglangxxx <541217492@qq.com>
2019-02-12 13:51:45 +0800

    SpringMvc入门

---
+++ SpringMvc_Demo1/src/com/springmvc/controller/ItemController.java
@@ -0,0 +1,31 @@
+package com.springmvc.controller;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.springmvc.pojo.Items;
+
+@Controller
+public class ItemController {
+
+	@RequestMapping(value="/item/itemlist.action")
+	public ModelAndView itemlist() {
+		// 创建页面需要显示的商品数据
+		List<Items> list = new ArrayList<>();
+		list.add(new Items(1, "1华为 荣耀8", 2399f, new Date(), "质量好！1"));
+		list.add(new Items(2, "2华为 荣耀8", 2399f, new Date(), "质量好！2"));
+		list.add(new Items(3, "3华为 荣耀8", 2399f, new Date(), "质量好！3"));
+		list.add(new Items(4, "4华为 荣耀8", 2399f, new Date(), "质量好！4"));
+		list.add(new Items(5, "5华为 荣耀8", 2399f, new Date(), "质量好！5"));
+		list.add(new Items(6, "6华为 荣耀8", 2399f, new Date(), "质量好！6"));
+		ModelAndView mav = new ModelAndView();
+		mav.addObject("itemList", list);
+		mav.setViewName("itemList");
+		return mav;		
+	}
+}
